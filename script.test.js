77660523293bc637071da484cbc766f73e59db1b
Hector Cisneros <hectorxx0xx@gmail.com>
2026-02-12 16:20:25 -0500

    Update script.test.js

--- script.test.js
+++ script.test.js
@@ -2,5 +2,6 @@ import { test, expect } from 'vitest'
 import { sum } from './script.js'
 
 test('suma incorrecta', () => {
-    expect(sum(2, 2)).toBe(5);  // Este test fallará intencionalmente
+    expect(sum(3, 2)).toBe(5);  // Este test fallará intencionalmente
+
 });
